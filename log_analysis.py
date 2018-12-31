b7d64ff93c08cd9988e9c2332277f148a04feeec
Wilmerben <sirwilmer@gmail.com>
2018-12-30 19:22:19 -0500

    Fix docstrings and removed empty lines

--- log_analysis.py
+++ log_analysis.py
@@ -5,39 +5,39 @@ import psycopg2
 DBName = "news"
 
 
-# Returns three popular articles.
 
 def get_popular_articles():
+    '''Prints the top three popular articles.
 
+    Prints the top three most viewed articles. Includes the title and
+    number of views, ordered by view descending.
+    '''
+    
     query = 'SELECT title, views FROM articles_log ORDER BY views DESC LIMIT 3'
-
     db = psycopg2.connect(database=DBName)
-
     cursor = db.cursor()
-
     cursor.execute(query)
-
     articles = cursor.fetchall()
-
     db.close()
-
+    
     print ('\n')
-
     print ('Top Three articles:')
-
     print ('\n')
-
+    
     for article in articles:
-
         print ('"%s" => %s views' %(article[0], article[1]))
-
+        
     return
 
 
-# Print most popular authors.
-
 def get_popular_authros():
+    '''Print most popular authors.
 
+    Prints the most popular authors in descending order by combined
+    views of each of their articles. Prints the author's name and total
+    views.
+    '''
+    
     query = '''SELECT authors.name as author, total_views FROM authors,
 
     (SELECT articles_log.author_id as author_id, SUM(articles_log.views)
@@ -47,59 +47,49 @@ def get_popular_authros():
     ORDER BY total_views DESC) as subq WHERE authors.id =
 
     subq.author_id;'''
-
+    
     db = psycopg2.connect(database=DBName)
-
     cursor = db.cursor()
-
     cursor.execute(query)
-
     authors = cursor.fetchall()
-
     db.close()
-
+    
     print ('\n')
-
     print ('Most popular article authors: ')
-
     print ('\n')
-
+    
     for author in authors:
-
         print ('%s => %s views' %(author[0], author[1]))
-
+        
     return
 
 
-# Prints days which lead to mare than 1% error.
-
 def get_days_percent_error():
+    '''Prints days which lead to mare than 1% error.
 
-    query = '''SELECT date, percent FROM (SELECT to_char(time,'MON DD
-    YYYY') as date, round(100.0*SUM(case log.status when '404 NOT FOUND'
-    then 1 else 0 end)/count(log.status),2) as percent FROM log GROUP BY
-    date ORDER BY percent DESC) as subq WHERE percent>1.00;'''
+    Prints the day and perecentage in which requests lead to more than
+    1% of the errors. Aggregated by the '404 NOT FOUND' status codes collected.
+    '''
+
+    query = '''SELECT date,
+    percent FROM (SELECT to_char(time,'MON DD YYYY') as date,
+    round(100.0*SUM(case log.status when '404 NOT FOUND' then 1 else 0
+    end)/count(log.status),2) as percent FROM log GROUP BY date ORDER BY
+    percent DESC) as subq WHERE percent>1.00;'''
 
     db = psycopg2.connect(database=DBName)
-
     cursor = db.cursor()
-
     cursor.execute(query)
-
     errors = cursor.fetchall()
-
     db.close()
-
+    
     print ('\n')
-
     print ('Days which requests lead to more than 1% of the errors:')
-
     print ('\n')
 
     for error in errors:
-
-        print ('%s => %s%% errors' %(error[0], serror[1]))
-
+        print ('%s => %s%% errors' %(error[0], error[1]))
+        
     return
 
 
